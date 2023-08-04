4455bb8a3852797588591072abbc160841be370f
Saiful Islam <saifulsaif321@gmail.com>
2023-08-04 23:06:22 +0600

    firebase integration

--- src/main/java/com/saiful/blogapplication/controller/WeatherController.java
+++ src/main/java/com/saiful/blogapplication/controller/WeatherController.java
@@ -1,5 +1,8 @@
 package com.saiful.blogapplication.controller;
 
+import com.saiful.blogapplication.model.Test;
+import com.saiful.blogapplication.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class WeatherController {
 
+    @Autowired
+    private TestService testService;
+
     @RequestMapping(value = "/weather", method = RequestMethod.GET)
     public String WeatherValue() {
         return "Weather : 35 celcius";
+    }
+
+    @RequestMapping(value = "/firebase", method = RequestMethod.GET)
+    public Test GetTest() {
+        return testService.getTest();
     }
 
 }
---
+++ src/main/java/com/saiful/blogapplication/firebase/FirebaseInitializer.java
@@ -0,0 +1,36 @@
+package com.saiful.blogapplication.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void initFirebase() {
+
+        FileInputStream serviceAccount;
+
+        try {
+            serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
