5e2e4a9afb0c09072295540be9521809218fe6c5
Saiful Islam <saifulsaif321@gmail.com>
2023-08-31 21:41:03 +0600

    database connection (#1)
    * firebase integration

---
+++ src/main/java/com/saiful/blogapplication/model/Test.java
@@ -0,0 +1,12 @@
+package com.saiful.blogapplication.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    public String Name;
+    public String Address;
+}
